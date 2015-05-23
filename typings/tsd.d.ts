23429cd8b5e7445c1d790a1b51736ce2b7b878bb
Peter Grman <peter.grman@gmail.com>
2015-05-23 14:45:18 +0200

    Add missing jquery.d.ts

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="meteor/meteor.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
