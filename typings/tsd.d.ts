a5e644d38b7d005017acbecca7d24aba19696b24
Peter Grman <peter.grman@gmail.com>
2015-05-23 14:41:20 +0200

    Add all packages

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="angular-material/angular-material.d.ts" />
+/// <reference path="angular-meteor/angular-meteor.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="meteor/meteor.d.ts" />
