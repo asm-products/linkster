6b712999e7db0fe32ffc56724481d9ed4e6672f8
Peter Grman <peter.grman@gmail.com>
2015-05-23 14:47:50 +0200

    initialize new folder structure

--- client/main.ts
+++ client/main.ts
@@ -1,23 +1,19 @@
-if (Meteor.isClient) {
+/// <reference path="../typings/tsd.d.ts" />
+
+module linkster.client {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template['hello'].helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template['hello'].events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
---
+++ server/main.ts
@@ -0,0 +1,7 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+module linkster.server {
+  Meteor.startup(() => {
+    // code to run on server at startup
+  });
+}
